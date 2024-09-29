e74a9e906a86bb05a092ae5cfab4425324156abe
Larisa <larissaatmasova@mail.ru>
2024-09-29 23:12:31 +0500

    add main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from src.widget import mask_account_card,get_date
-number = ''
+from src.widget import mask_account_card, get_date
+
+number = ""
 print("Введите тип и номер карты или введите счет:")
 user_card_or_score = input()
 print(mask_account_card(user_card_or_score))
+
 print("Введите дату:")
 user_date = input()
 print(get_date(user_date))
