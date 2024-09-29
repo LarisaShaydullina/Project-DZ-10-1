53ffcd6366be98a230eed23a35aab911b09f9492
Larisa <larissaatmasova@mail.ru>
2024-09-29 21:09:31 +0500

    add src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Функция, которая принимает на вход номер карты в виде числа и
+    возвращает маску номера по правилу XXXX XX** **** XXXX
+    """
+    mask_card_number = card_number[0:4] + " " + card_number[4:6] + "**" + " " + "****" + card_number[-4:]
+    return mask_card_number
+
+
+def get_mask_account(account: str) -> str:
+    """
+    Функция, которая принимает на вход номер счета в виде числа и
+    возвращает маску номера по правилу **XXXX
+    """
+    mask_account = "**" + account[-4:]
+    return mask_account
