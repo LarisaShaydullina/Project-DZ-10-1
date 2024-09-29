951cb9c25e07388f5a7e02e59640393c3af3d60d
Larisa <larissaatmasova@mail.ru>
2024-09-29 22:48:39 +0500

    add

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from src.masks import get_mask_card_number, get_mask_account
-
-print("Введите номер карты в виде числа:")
-user_card_number = input()
-print(get_mask_card_number(user_card_number))
-
-print("Введите номер счета в виде числа:")
-user_account = input()
-print(get_mask_account(user_account))
+from src.widget import mask_account_card,get_date
+number = ''
+print("Введите тип и номер карты или введите счет:")
+user_card_or_score = input()
+print(mask_account_card(user_card_or_score))
+print("Введите дату:")
+user_date = input()
+print(get_date(user_date))
