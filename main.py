4dba8412804ffc94dc53f1bc3071eaede94b6f28
Larisa <larissaatmasova@mail.ru>
2024-10-05 18:26:13 +0500

    Домашнее задание по разделу 10.1, доработка

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from src.widget import mask_account_card, get_date
+from src.processing import filter_by_state, sort_by_date
+
+dictionary_id = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "20181014T08:21:33.419441"},
+]
 
 number = ""
 print("Введите тип и номер карты или введите счет:")
@@ -8,3 +16,11 @@ print(mask_account_card(user_card_or_score))
 print("Введите дату:")
 user_date = input()
 print(get_date(user_date))
+
+print("Введите параметр для ключа состояния операций state, успешные или отмененные (EXECUTED/CANCELED):")
+state = input()
+print(filter_by_state(dictionary_id, state))
+
+print("Введите метод сортировки операций, по убыванию или по возрастанию (True/False):")
+ascending = input()
+print(sort_by_date(dictionary_id, ascending))
--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,44 @@
 from src.masks import get_mask_card_number, get_mask_account
 
+cards_payment_system = [
+    'Visa', 'Mastercard', 'Maestro', 'МИР', 'UnionPay', 'AmericanExpress',
+    'Discover', 'RuPay', 'Uzcard', 'Humo', 'Shetab', 'ArmenianCard',
+    'BankNet', 'Troy', 'Белкарт'
+]
 
 def mask_account_card(card_or_score: str) -> str:
     """
     Функция, которая возвращает строку с замаскированным номером карты или счета
     """
-    if "Счет" in card_or_score:
+    if "Счет" in card_or_score or 'счет' in card_or_score or 'СЧЕТ' in card_or_score:
         number_of_account = ""
         type_of_account = ""
         for index_account in card_or_score:
             if index_account.isdigit():
                 number_of_account += index_account
-            else:
+            elif index_account.isalpha():
                 type_of_account += index_account
-        return f"{type_of_account}{get_mask_account(number_of_account)}"
+        if (get_mask_account(number_of_account) != "Некорректное значение номера счета"):
+            return f"{type_of_account} {get_mask_account(number_of_account)}"
+        else:
+            return get_mask_account(number_of_account)
     else:
         number_of_card = ""
         type_of_card = ""
+        count = 0
         for index_card in card_or_score:
             if index_card.isdigit():
                 number_of_card += index_card
-            else:
+            elif index_card.isalpha():
                 type_of_card += index_card
-        return f"{type_of_card}{get_mask_card_number(number_of_card)}"
+        for i in cards_payment_system:
+            if type_of_card == i:
+                count = 1
+        if (get_mask_card_number(number_of_card) != "Некорректное значение номера карты") and (count == 1):
+            return f"{type_of_card} {get_mask_card_number(number_of_card)}"
+        else:
+            return 'Некорректное значение номера счета, номера карты или типа платежной системы'
+
 
 
 def get_date(date: str) -> str:
@@ -30,7 +46,10 @@ def get_date(date: str) -> str:
     Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ"а
     """
-    date = date.split("T")[0]
-    year, month, day = date.split("-")
-    correct_date = f"{day}.{month}.{year}"
-    return correct_date
+    if len(date) == 26 and date[10] == 'T' and date[4] == '-' and date[7] == '-':
+        date = date.split("T")[0]
+        year, month, day = date.split("-")
+        correct_date = f"{day}.{month}.{year}"
+        return correct_date
+    else:
+        return ('Некорректное значение')
