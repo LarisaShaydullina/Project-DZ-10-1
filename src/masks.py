3546db3ebbe0ac76c0916d7394b1591de7d8f27e
Larisa <larissaatmasova@mail.ru>
2024-11-03 00:35:54 +0500

    Домашнее задание по разделу 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,26 @@
+import logging
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/masks.log", mode="w", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """
     Функция, которая принимает на вход Номер карты, и
     возвращает маску номера по правилу XXXX XX** **** XXXX
     """
     if 16 <= len(card_number) <= 20 and card_number.isdigit():
+        logger.info(f"Введенный номер карты: {card_number}, содержит от 16 до 20 цифр")
         mask_card_number = card_number[0:4] + " " + card_number[4:6] + "**" + " " + "****" + " " + card_number[-4:]
+        logger.info(f"Замаскированный номер карты: {mask_card_number}")
         return mask_card_number
     else:
+        logger.error("Произошла ошибка: некорректное значение номера карты")
         return "Некорректное значение номера карты"
 
 
@@ -16,7 +30,16 @@ def get_mask_account(account: str) -> str:
     возвращает маску номера по правилу **XXXX
     """
     if len(account) == 20:
+        logger.info(f"Введенный номер счета: {account}, содержит 20 цифр")
         mask_account = "**" + account[-4:]
+        logger.info(f"Замаскированный номер счета: {mask_account}")
         return mask_account
     else:
+        logger.error("Произошла ошибка: некорректное значение номера счета")
         return "Некорректное значение номера счета"
+
+
+if __name__ == "__main__":
+    mask_card_result = get_mask_card_number("1111222233334444")
+    mask_account_result = get_mask_account("12345678910111213141")
+    print(f"Маска карты: {mask_card_result} \nМаска счета: {mask_account_result}")
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,40 @@
 import json
+import logging
 from json import JSONDecodeError
 from typing import Any
 from src.external_api import currency_conversion
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/utils.log", mode="w", encoding="utf-8")
+file_handler.setLevel(logging.DEBUG)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def financial_transactions(path_to_file: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о
     финансовых транзакциях."""
     try:
+        logger.info(f"Получение данных из файла {path_to_file}")
         print(f"Получение данных из файла {path_to_file}")
         with open(path_to_file, encoding="utf-8") as financial_file:
             try:
                 transactions = json.load(financial_file)
             except JSONDecodeError:
+                logger.error("Ошибка файла с транзакциями")
                 print("Ошибка файла с транзакциями")
                 return []
         if not isinstance(transactions, list):
+            logger.error("Список транзакций пуст")
             print("Список транзакций пуст")
             return []
+        logger.info("Список словарей с данными о финансовых транзакциях")
         print("Список словарей с данными о финансовых транзакциях")
         return transactions
     except FileNotFoundError:
+        logger.error("Файл с транзакциями не найден")
         print("Файл с транзакциями не найден")
         return []
 
@@ -29,9 +43,11 @@ def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     if trans["operationAmount"]["currency"]["code"] == currency:
         amount = trans["operationAmount"]["amount"]
-        print("Код валюты в транзакции RUB")
+        logger.info("Код валюты в транзакции - RUB")
+        print("Код валюты в транзакции - RUB")
     else:
         amount = currency_conversion(trans)
+        logger.info("Код валюты транзакции не RUB, произведена конвертация")
         print("Код валюты транзакции не RUB, произведена конвертация")
     return amount
 
